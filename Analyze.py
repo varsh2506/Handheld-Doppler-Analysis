de28cee7b4407b0e602aa698db1206f6867fc9fd
MehakArora <mehakarora23@gmail.com>
2019-10-15 23:10:40 +0530

    Cumsum

--- Analyze.py
+++ Analyze.py
@@ -12,10 +12,14 @@ Pxx, freqs, times, im = plt.specgram(beat,Fs=fs)
 
 intervals = len(times);
 block = np.int(np.ceil(intervals/4));
-TI1 = np.sum(Pxx[0:block-1,:], axis = 1);
-TI2 = np.sum(Pxx[block:2*block-1,:], axis = 1)
-TI3 = np.sum(Pxx[2*block:3*block-1,:], axis = 1)
-TI4 = np.sum(Pxx[3*block:intervals-1,:], axis = 1)
+ti1 = np.zeros(int(len(Pxx)))
+ti2 = np.zeros(int(len(Pxx)))
+ti3 = np.zeros(int(len(Pxx)))
+ti4 = np.zeros(int(len(Pxx)))
+ti1 = np.sum(Pxx[:,0:block-1], axis = 1);
+ti2 = np.sum(Pxx[:,block:2*block-1], axis = 1)
+ti3 = np.sum(Pxx[:,2*block:3*block-1], axis = 1)
+ti4 = np.sum(Pxx[:,3*block:intervals-1], axis = 1)
 
 
 #%%
@@ -53,3 +57,78 @@ plt.show()
 #    stat_range=stat_max-stat_min
 #    stat_mean[0]=np.mean(mean_array[0:2])
 #    stat_mean[1]=np.mean(mean_array[2:]
+
+#%%
+print(len(TI1))
+cumsum1= np.cumsum(ti1)
+cumsum1 = cumsum1/max(cumsum1)
+flag1=0
+flag2=0
+for i in range(int(len(cumsum1))):
+    if(cumsum1[i]>0.95):
+        percent95_1 = freqs[i]
+        break
+    elif(cumsum1[i]>0.5 and flag1==0):
+        percent50_1 = freqs[i]
+        flag1=1
+    elif(cumsum1[i]>0.05 and flag2==0):
+        percent5_1=freqs[i]
+        flag2=1;
+print(percent95_1)
+print(percent50_1)
+print(percent5_1)
+
+#%%
+cumsum2= np.cumsum(ti2)
+cumsum2 = cumsum2/max(cumsum2)
+flag1=0
+flag2=0
+for i in range(int(len(cumsum2))):
+    if(cumsum2[i]>0.95):
+        percent95_2 = freqs[i]
+        break
+    elif(cumsum2[i]>0.5 and flag1==0):
+        percent50_2 = freqs[i]
+        flag1=1
+    elif(cumsum2[i]>0.05 and flag2==0):
+        percent5_2=freqs[i]
+        flag2=1;
+print(percent95_2)
+print(percent50_2)
+print(percent5_2)
+#%%
+cumsum3= np.cumsum(ti3)
+cumsum3 = cumsum3/max(cumsum3)
+flag1=0
+flag2=0
+for i in range(int(len(cumsum3))):
+    if(cumsum3[i]>0.95):
+        percent95_3 = freqs[i]
+        break
+    elif(cumsum3[i]>0.5 and flag1==0):
+        percent50_3 = freqs[i]
+        flag1=1
+    elif(cumsum3[i]>0.05 and flag2==0):
+        percent5_3=freqs[i]
+        flag2=1;
+print(percent95_3)
+print(percent50_3)
+print(percent5_3)
+#%%
+cumsum4= np.cumsum(ti4)
+cumsum4 = cumsum4/max(cumsum4)
+flag1=0
+flag2=0
+for i in range(int(len(cumsum4))):
+    if(cumsum4[i]>0.95):
+        percent95_4 = freqs[i]
+        break
+    elif(cumsum4[i]>0.5 and flag1==0):
+        percent50_4 = freqs[i]
+        flag1=1
+    elif(cumsum4[i]>0.05 and flag2==0):
+        percent5_4=freqs[i]
+        flag2=1;
+print(percent95_4)
+print(percent50_4)
+print(percent5_4)
