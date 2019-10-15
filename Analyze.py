052477aaf2e32453b13586f69e954e6bf9f93573
MehakArora <mehakarora23@gmail.com>
2019-10-15 22:39:53 +0530

    Slicing

--- Analyze.py
+++ Analyze.py
@@ -1,41 +1,55 @@
 
 from scipy.io import wavfile
-fs, data = wavfile.read('cw_adiveppa_4_19.wav')
+fs, data = wavfile.read('HandheldRecorded\\cw_ajay_1_17_beat.wav')
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def analyze(data,x,y,fs):
-    beat=data[x:y]
-    Pxx, freqs, times, im = plt.specgram(beat, NFFT=1024, Fs=fs,noverlap=256)
-    plt.show()
-    Pxx=10*np.log10(Pxx)
-    freq=np.zeros((len(freqs),len(times)))
-    max_array=np.zeros(len(times))
-    min_array=np.zeros(len(times))
-    range_array=np.zeros(len(times))
-    mean_array=np.zeros(len(times))
-    temp=np.zeros(len(freq))
-    stat_max=np.zeros(2)
-    stat_min=np.zeros(2)
-    stat_range=np.zeros(2)
-    stat_mean=np.zeros(2)
-    for j in range(len(times)):
-        for i in range(len(freqs)):
-            if(Pxx[i][j]>=30):
-                freq[i][j]=freqs[i]
-        max_array[j]=max(freq[:,j])
-        temp=freq[:,j]
-        temp=temp[np.nonzero(temp)]
-        min_array[j]=min(temp)
-        range_array[j]=max_array[j]-min_array[j]
-        mean_array[j]=np.mean(temp)
-    stat_max[0]=max(max_array[0:2])
-    stat_max[1]=max(max_array[2:])
-    stat_min[0]=min(min_array[0:2])
-    stat_min[1]=min(min_array[2:])
-    stat_range=stat_max-stat_min
-    stat_mean[0]=np.mean(mean_array[0:2])
-    stat_mean[1]=np.mean(mean_array[2:])
-    return stat_max,stat_min,stat_range,stat_mean
-a,b,c,d=analyze(data,72860,76740,fs)
+beat=data;
+seglength = len(beat)/4;
+windowa = np.ones(np.int(len(beat)/4));
+Pxx, freqs, times, im = plt.specgram(beat,Fs=fs)
+
+intervals = len(times);
+block = np.int(np.ceil(intervals/4));
+TI1 = np.sum(Pxx[0:block-1,:], axis = 1);
+TI2 = np.sum(Pxx[block:2*block-1,:], axis = 1)
+TI3 = np.sum(Pxx[2*block:3*block-1,:], axis = 1)
+TI4 = np.sum(Pxx[3*block:intervals-1,:], axis = 1)
+
+
+#%%
+print(Pxx[:,0:12])
+
+
+#%%
+
+plt.show()
+#    Pxx=10*np.log10(Pxx)
+#    freq=np.zeros((len(freqs),len(times)))
+#    max_array=np.zeros(len(times))
+#    min_array=np.zeros(len(times))
+#    range_array=np.zeros(len(times))
+#    mean_array=np.zeros(len(times))
+#    temp=np.zeros(len(freq))
+#    stat_max=np.zeros(2)
+#    stat_min=np.zeros(2)
+#    stat_range=np.zeros(2)
+#    stat_mean=np.zeros(2)
+#    for j in range(len(times)):
+#        for i in range(len(freqs)):
+#            if(Pxx[i][j]>=30):
+#                freq[i][j]=freqs[i]
+#        max_array[j]=max(freq[:,j])
+#        temp=freq[:,j]
+#        temp=temp[np.nonzero(temp)]
+#        min_array[j]=min(temp)
+#        range_array[j]=max_array[j]-min_array[j]
+#        mean_array[j]=np.mean(temp)
+#    stat_max[0]=max(max_array[0:2])
+#    stat_max[1]=max(max_array[2:])
+#    stat_min[0]=min(min_array[0:2])
+#    stat_min[1]=min(min_array[2:])
+#    stat_range=stat_max-stat_min
+#    stat_mean[0]=np.mean(mean_array[0:2])
+#    stat_mean[1]=np.mean(mean_array[2:]
